7f0b21e8940581c6a0d4c5c2b9d5bba80f3e5bed
EgorGotbit <egor.nikolaev@gotbit.io>
2024-10-14 23:38:01 +0300

    feat: add username from args, start message, end message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('init')
+const main = async () => {
+  // Get username from args
+  const args = process.argv.slice(2);
+
+  if (!args || args.length < 1) {
+    throw new Error("Invalid input");
+  }
+
+  const username = args[0].split("=")[1];
+
+  console.log(`Welcome to the File Manager, ${username}!`);
+
+  process.on("SIGINT", () => {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    process.exit();
+  });
+
+  process.stdin.on("data", (data) => {
+    if (data.toString().startsWith(".exit")) {
+      process.kill(process.pid, "SIGINT");
+      return;
+    }
+  });
+
+  process.stdin.on("error", (err) => console.error(err));
+  process.stdin.on("end", () => console.log("ended"));
+};
+
+await main();
